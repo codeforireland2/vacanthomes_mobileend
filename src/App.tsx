3ead367cd173c38b492e0b832e640968890babd2
George Kemp <georgekemp94@gmail.com>
2018-09-30 14:31:54 +0100

    Adds Quick Example Application Code

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,63 @@
-import * as React from 'react';
-import './App.css';
+import { Icon, Modal, NavBar } from "antd-mobile";
+import * as React from "react";
+import "./App.css";
 
-class App extends React.Component {
+interface IAppState {
+  isAboutLaunched: boolean;
+}
+
+class App extends React.Component<{}, IAppState> {
+  constructor(props: {}) {
+    super(props);
+    this.showAbout = this.showAbout.bind(this);
+    this.state = {
+      isAboutLaunched: false,
+    }
+
+  }
   public render() {
     return (
       <div className="App">
-        Vacant Homes Mobile UI
+        <NavBar
+          mode="dark"
+          rightContent={[
+            <Icon 
+            key="0"
+             type="question-circle" 
+             style={{ marginRight: "16px" }}
+             onClick={this.showAbout} 
+             />,
+          ]}
+        >
+          Vacant Homes
+        </NavBar>
+        <Modal
+          visible={this.state.isAboutLaunched}
+          transparent={true}
+          maskClosable={false}
+          onClose={this.closeAbout}
+          title="About"
+          footer={[{ text: 'Ok', onPress: () => { this.closeAbout(); } }]}
+        >
+          <div style={{ height: 50 }}>
+          Vacant Homes is currently in development by Code for Ireland
+          </div>
+        </Modal>
       </div>
     );
+  }
+
+  private showAbout(e: React.MouseEvent<SVGSVGElement>) {
+    e.preventDefault();
+    this.setState({
+      isAboutLaunched: true,
+    })
+  }
+
+  private closeAbout() {
+    this.setState({
+      isAboutLaunched: false,
+    });
   }
 }
 
